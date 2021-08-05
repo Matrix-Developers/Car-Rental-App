6f9c8af52857e5355fba3b170cf8eb3ddf77a937
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-08-05 17:50:09 -0300

    Adicionado Nugget do Fluent Assertion e atualizado packages do MSTest

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Tests/VeiculoModule/VeiculoTest.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Tests/VeiculoModule/VeiculoTest.cs
@@ -4,7 +4,7 @@ using System;
 namespace LocadoraDeVeiculos.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class VeiculoTest
     {
         [TestMethod]
         public void TestMethod1()
