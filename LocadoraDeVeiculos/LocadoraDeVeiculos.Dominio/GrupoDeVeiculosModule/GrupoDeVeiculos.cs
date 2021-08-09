4d052d0f63b9fd46327ee54e854e298b0b0b1926
Iohan Voigt <iohaneduardo1@gmail.com>
2021-08-09 16:32:14 -0300

    Programado o validador do Grupo De Veículos e os testes do domíno de Grupo de Veículos.

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/GrupoDeVeiculosModule/GrupoDeVeiculos.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/GrupoDeVeiculosModule/GrupoDeVeiculos.cs
@@ -1,11 +1,33 @@
 ﻿namespace LocadoraDeVeiculos.Dominio.GrupoDeVeiculosModule
 {
-    class GrupoDeVeiculos
+    public class GrupoDeVeiculos
     {
-        string nome;
-        float taxaPlanoDiario;
-        float taxaKmControlado;
-        int quantidadeQuilometrosKmControlado;
-        float taxaKmLivre;
+        public string nome;
+        public float taxaPlanoDiario;
+        public float taxaKmControlado;
+        public int quantidadeQuilometrosKmControlado;
+        public float taxaKmLivre;
+
+        public string Validar()
+        {
+            string resultadoValidacao = "VALIDO";
+
+            if (this.nome.Length <= 0)
+                resultadoValidacao  = "O nome não pode ser nulo\n";
+
+            if (this.taxaKmControlado <= 0f)
+                resultadoValidacao += "A taxa do Quilometro Controlado não pode ser nula nem negativa\n";
+
+            if (this.taxaPlanoDiario <=0f)
+                resultadoValidacao += "A taxa do Plano Diário não pode ser nula nem negativa\n";
+
+            if (this.taxaKmLivre <= 0f)
+                resultadoValidacao += "A taxa do Quilometro Livre não pode ser nula nem negativa\n";
+
+            if (this.quantidadeQuilometrosKmControlado <= 0)
+                resultadoValidacao += "A quantidade de quilômetros não pode ser nulo nem negativo";
+
+            return resultadoValidacao;
+        }
     }
 }
--- LocadoraDeVeiculos/LocadoraDeVeiculos.Tests/GrupoDeVeiculosModule/GrupoDeVeiculosTest.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Tests/GrupoDeVeiculosModule/GrupoDeVeiculosTest.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LocadoraDeVeiculos.Dominio.GrupoDeVeiculosModule;
+using FluentAssertions;
 using System;
 
 namespace LocadoraDeVeiculos.Tests.GrupoDeVeiculosModule
@@ -7,8 +9,42 @@ namespace LocadoraDeVeiculos.Tests.GrupoDeVeiculosModule
     public class GrupoDeVeiculosTest
     {
         [TestMethod]
-        public void DeveCriarGrupoComKmLivre()
+        public void DeveCriarDominioCorreto()
         {
+            GrupoDeVeiculos grupoDeVeiculos = new GrupoDeVeiculos();
+            string nome = "nome";
+            float taxaPlanoDiario = 12.50f;
+            float taxaKmControlado = 25.73f;
+            int quantidadeQuilometrosKmControlado = 200;
+            float taxaKmLivre = 13.99f;
+
+            grupoDeVeiculos.nome = nome;
+            grupoDeVeiculos.taxaPlanoDiario = taxaPlanoDiario;
+            grupoDeVeiculos.taxaKmControlado = taxaKmControlado;
+            grupoDeVeiculos.quantidadeQuilometrosKmControlado = quantidadeQuilometrosKmControlado;
+            grupoDeVeiculos.taxaKmLivre = taxaKmLivre;
+
+            Assert.AreEqual("VALIDO", grupoDeVeiculos.Validar());
+        }
+
+        [TestMethod]
+        public void DeveCriarDominioIncorreto()
+        {
+            GrupoDeVeiculos grupoDeVeiculos = new GrupoDeVeiculos();
+            string nome = "";
+            float taxaPlanoDiario = 0f;
+            float taxaKmControlado = 0f;
+            int quantidadeQuilometrosKmControlado = 0;
+            float taxaKmLivre = 0f;
+
+            grupoDeVeiculos.nome = nome;
+            grupoDeVeiculos.taxaPlanoDiario = taxaPlanoDiario;
+            grupoDeVeiculos.taxaKmControlado = taxaKmControlado;
+            grupoDeVeiculos.quantidadeQuilometrosKmControlado = quantidadeQuilometrosKmControlado;
+            grupoDeVeiculos.taxaKmLivre = taxaKmLivre;
+
+            Assert.AreEqual("O nome não pode ser nulo\nA taxa do Quilometro Controlado não pode ser nula nem negativa\nA taxa do Plano Diário não pode ser nula nem negativa\nA taxa do Quilometro Livre não pode ser nula nem negativa\nA quantidade de quilômetros não pode ser nulo nem negativo", 
+                            grupoDeVeiculos.Validar());
         }
     }
 }
