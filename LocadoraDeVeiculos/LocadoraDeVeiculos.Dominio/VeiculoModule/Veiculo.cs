6d49a27ce19c00cec5b3f5aa7201a37b93554fbe
ViniJordao <viniciusjordao02@gmail.com>
2021-08-09 15:12:10 -0300

    Criando campos da classe veiculo

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/VeiculoModule/Veiculo.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/VeiculoModule/Veiculo.cs
@@ -1,7 +1,25 @@
-﻿
+﻿using System;
+
+
 namespace LocadoraDeVeiculos.Dominio.VeiculoModule
 {
     public class Veiculo
     {
+        public string Modelo;
+        public string GrupoVeiculos;
+        public string Placa;
+        public string Chassi;
+        public string Marca;
+        public string Cor;
+        public string TipoCombustivel;
+        public string CapacidadeTanque;
+        public int Ano;
+        public string Kilometragem;
+        public string NumeroPortas;
+        public int CapacidadePessoas;
+        public string TamanhoPortaMala;
+        public bool TemArCondicionado;
+        public bool TemDirecaoHidraulica;
+        public bool TemFreiosAbs;
     }
 }
