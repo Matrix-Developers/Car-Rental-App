631904da65c88b2ac7d203e935870b1d5b66c98b
Iohan Voigt <iohaneduardo1@gmail.com>
2021-08-10 11:07:09 -0300

    Desenvolvido o Dominio de Serviços e seus testes unitários

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/SevicosModule/Servico.cs
@@ -0,0 +1,30 @@
+﻿namespace LocadoraDeVeiculos.Dominio.SevicosModule
+{
+    public class Servico
+    {
+        private string nome;
+        private string tipo;
+        private float valor;
+
+        public Servico(string nome, string tipo, float valor)
+        {
+            this.nome = nome;
+            this.tipo = tipo;
+            this.valor = valor;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public float Valor { get => valor; set => valor = value; }
+
+        public string Validar() 
+        {
+            string resultadoValidacao = "VALIDO";
+            if (this.nome.Length <= 0)
+                resultadoValidacao = "O nome não pode ser nulo\n";
+            if (this.valor <= 0)
+                resultadoValidacao += "O valor não pode ser nulo ou negativo";
+            return resultadoValidacao;
+        }
+    }
+}
---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Tests/SevicoModule/ServicosTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LocadoraDeVeiculos.Dominio.SevicosModule;
+
+namespace LocadoraDeVeiculos.Tests.SevicoModule
+{
+
+    [TestClass]
+    public class ServicosTest
+    {
+
+        [TestMethod]
+        public void DeveCriarServicoCorreto()
+        {
+            Servico servico = new Servico("nome","tipo",100f);
+            Assert.AreEqual("VALIDO",servico.Validar());
+        }
+
+        [TestMethod]
+
+        public void DeveCriarServicoIncorreto()
+        {
+            Servico servico = new Servico("", "", 0f);
+            Assert.AreEqual("O nome não pode ser nulo\nO valor não pode ser nulo ou negativo", servico.Validar());
+        }
+    }
+}
