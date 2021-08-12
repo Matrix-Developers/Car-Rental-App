8da6671da3425245273661f3feec1f1351ae1110
ViniJordao <viniciusjordao02@gmail.com>
2021-08-12 16:07:32 -0300

    Adcionado as queries na classe ControladorCliente

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Controladores/ClientesModule/ControladorClientes.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Controladores/ClientesModule/ControladorClientes.cs
@@ -6,7 +6,71 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Controladores.ClientesModule
 {
-    class Class1
+    public class ControladorCliente
     {
-    }
+        #region Queries
+            private const string sqlInserirClientes =
+            @"
+               INSERT INTO [TBCLIENTE]
+            (
+	            [NOME],
+	            [REGISTROUNICO],
+	            [ENDERECO],
+	            [TELEFONE],
+	            [EMAIL],
+	            [EHPESSOAFISICA],
+	            [CNH],
+	            [VALIDADECNH]
+            )
+            VALUES
+            (
+	            @NOME,
+	            @REGISTROUNICO,
+	            @ENDERECO,
+	            @EMAIL,
+	            @TELEFONE,
+	            @EHPESSOAFISICA,
+	            @CNH,
+	            @VALIDADECNH
+            )";
+
+		private const string sqlEditarClientes =
+		@"
+				UPDATE [TBCLIENTE] 
+				 SET
+					[NOME] = @NOME,
+					[REGISTROUNICO] = @REGISTROUNICO,
+					[ENDERECO] = @ENDERECO,
+					[TELEFONE] = @TELEFONE,
+					[EMAIL] = @EMAIL,
+					[EHPESSOAFISICA] = @EHPESSOAFISICA,
+					[CNH] = @CNH,
+					[VALIDADECNH] = @VALIDADECNH
+				WHERE [ID] = @ID;
+            )";
+
+		private const string sqlExcluirClientes =
+		@"
+				DELETE FROM [TBCLIENTE] WHERE [ID] = @ID
+			";
+
+		private const string sqlSelecionarTodosClientes =
+		@"
+			SELECT * FROM [TBCLIENTE]
+			";
+
+		private const string sqlSelecionarPorId =
+		@"
+			SELECT * FROM [TBCLIENTE] WHERE [ID] = @ID;
+			";
+
+			private const string sqlExisteCliente =
+			@"SELECT 
+                COUNT(*) 
+            FROM 
+                [TBCLIENTE]
+            WHERE 
+                [ID] = @ID";
+		#endregion
+	}
 }
