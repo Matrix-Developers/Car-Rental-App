d7a14b1a03477475c3366aa95909e25c49520e1b
ViniJordao <viniciusjordao02@gmail.com>
2021-08-12 15:52:02 -0300

    criado controlador de clientes

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Tests/ClienteModule/ClienteControladorTest.cs
@@ -0,0 +1,23 @@
+﻿using LocadoraDeVeiculos.Controladores.ClientesModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LocadoraDeVeiculos.Tests.ClienteModule
+{
+    [TestClass]
+    [TestCategory("Controladores")]
+    public class ClienteControladorTest
+    {
+        ControladorClientes controlador = null;
+
+        public ControladorClientesTest()
+        {
+            controlador = new ControladorClientes();
+         
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
