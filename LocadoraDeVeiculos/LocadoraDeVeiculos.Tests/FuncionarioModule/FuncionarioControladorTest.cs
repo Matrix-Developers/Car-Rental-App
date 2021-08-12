de8fef5504d5ce141cdc3b248f896fb337cb2762
iohan <iohaneduardo1@gmail.com>
2021-08-12 17:32:53 -0300

    testes bugando

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Tests/FuncionarioModule/FuncionarioControladorTest.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Tests/FuncionarioModule/FuncionarioControladorTest.cs
@@ -1,13 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LocadoraDeVeiculos.Controladores.FuncionarioModule;
+using LocadoraDeVeiculos.Dominio.FuncionarioModule;
 
 namespace LocadoraDeVeiculos.Tests.FuncionarioModule
 {
+    [TestClass]
     class FuncionarioControladorTest
     {
+        Funcionario funcionario = new Funcionario(0, "Nome Teste", "954.746.736-04", "Endereco Funcionario", "4932518000", "teste@email.com", 001, "user acesso", new DateTime(2021, 01, 01), "Vendedor", 1000f);
+        ControladorFuncionario ctr = new ControladorFuncionario();
+
+        [TestMethod]
+        public void DeveInserirFuncionarioNoBanco()
+        {
+            ctr.InserirNovo(funcionario);
+        }
     }
 }
