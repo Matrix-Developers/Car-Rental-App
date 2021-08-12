27745c3fe23387506696f50d80d12a9aca945b24
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-08-12 14:45:02 -0300

    Classe ControladorGrupoDeVeiculos e seus metodos de CRUD

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Controladores/GrupoDeVeiculosModule/ControladorGrupoDeVeiculos.cs
@@ -0,0 +1,93 @@
+﻿using LocadoraDeVeiculos.Controladores.Shared;
+using LocadoraDeVeiculos.Dominio.GrupoDeVeiculosModule;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace LocadoraDeVeiculos.Controladores.GrupoDeVeiculosModule
+{
+    public class ControladorGrupoDeVeiculos : Controlador<GrupoDeVeiculos>
+    {
+        private const string sqlInserirGrupoDeVeiculos =
+            @"";
+
+        private const string sqlEditarGrupoDeVeiculos =
+            @" ";
+
+        private const string sqlExcluirGrupoDeVeiculos =
+            @" ";
+
+        private const string sqlSelecionarGrupoDeVeiculosPorId =
+            @" ";
+
+        private const string sqlSelecionarTodosGrupoDeVeiculoss =
+            @" ";
+
+        private const string sqlExisteGrupoDeVeiculos =
+            @" ";
+
+        public override string InserirNovo(GrupoDeVeiculos registro)
+        {
+            string resultadoValidacao = registro.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+            {
+                registro.Id = Db.Insert(sqlInserirGrupoDeVeiculos, ObtemParametrosGrupoDeVeiculos(registro));
+            }
+
+            return resultadoValidacao;
+        }
+
+        public override string Editar(int id, GrupoDeVeiculos registro)
+        {
+            string resultadoValidacao = registro.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+            {
+                registro.Id = id;
+                Db.Update(sqlEditarGrupoDeVeiculos, ObtemParametrosGrupoDeVeiculos(registro));
+            }
+
+            return resultadoValidacao;
+        }
+
+        public override bool Excluir(int id)
+        {
+            try
+            {
+                Db.Delete(sqlExcluirGrupoDeVeiculos, AdicionarParametro("ID", id));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override bool Existe(int id)
+        {
+            return Db.Exists(sqlExisteGrupoDeVeiculos, AdicionarParametro("ID", id));
+        }
+
+        public override GrupoDeVeiculos SelecionarPorId(int id)
+        {
+            return Db.Get(sqlSelecionarGrupoDeVeiculosPorId, ConverterEmGrupoDeVeiculos, AdicionarParametro("ID", id));
+        }
+
+        public override List<GrupoDeVeiculos> SelecionarTodos()
+        {
+            return Db.GetAll(sqlSelecionarTodosGrupoDeVeiculoss, ConverterEmGrupoDeVeiculos);
+        }
+
+        private Dictionary<string, object> ObtemParametrosGrupoDeVeiculos(GrupoDeVeiculos grupoDeVeiculos)
+        {
+            throw new NotImplementedException();
+        }
+
+        private GrupoDeVeiculos ConverterEmGrupoDeVeiculos(IDataReader reader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
