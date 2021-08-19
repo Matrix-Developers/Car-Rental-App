d55182e06e6749dc664410477bee628362366d22
ViniJordao <viniciusjordao02@gmail.com>
2021-08-19 16:57:54 -0300

    Tela de login funcionando com o usuário padrão

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.WindowsApp/Features/Login/TelaLogin.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace LocadoraDeVeiculos.WindowsApp.Features.Login
+{
+    partial class TelaLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textSenha = new System.Windows.Forms.TextBox();
+            this.textUsuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(239, 228);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirmar.TabIndex = 26;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(33, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 23);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Senha";
+            // 
+            // textSenha
+            // 
+            this.textSenha.Location = new System.Drawing.Point(154, 197);
+            this.textSenha.Name = "textSenha";
+            this.textSenha.PasswordChar = '*';
+            this.textSenha.Size = new System.Drawing.Size(160, 20);
+            this.textSenha.TabIndex = 24;
+            // 
+            // textUsuario
+            // 
+            this.textUsuario.Location = new System.Drawing.Point(154, 171);
+            this.textUsuario.Name = "textUsuario";
+            this.textUsuario.Size = new System.Drawing.Size(160, 20);
+            this.textUsuario.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(33, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 23);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(14, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(312, 43);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Locadora de Veículos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkGray;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(106, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 25);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Tela de Login";
+            // 
+            // TelaLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(338, 263);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textSenha);
+            this.Controls.Add(this.textUsuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TelaLogin";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textSenha;
+        private System.Windows.Forms.TextBox textUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.WindowsApp/Features/Login/TelaLogin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraDeVeiculos.WindowsApp.Features.Login
+{
+    public partial class TelaLogin : Form
+    {
+        Thread thread;
+        public TelaLogin()
+        {
+            InitializeComponent();
+        }
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            if (textUsuario.Text == "admin" && textSenha.Text == "admin")
+            {
+                MessageBox.Show("Bem vindo " + textUsuario.Text);
+                thread = new Thread(ChamarTelaPrincipal);
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
+
+                this.Hide();
+                TelaLogin login = new TelaLogin();
+                login.Close();
+               
+            }
+            else
+            {
+                MessageBox.Show("Login ou senha inválidos");
+            }
+        }
+        private void ChamarTelaPrincipal()
+        {
+            Application.Run(new TelaPrincipalForm()); ;
+        }
+    }
+    
+}
