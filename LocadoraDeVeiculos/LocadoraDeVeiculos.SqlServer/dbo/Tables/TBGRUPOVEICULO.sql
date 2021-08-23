b75901b54fbd686ea676c20f73397baadb16cad1
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-08-23 15:06:51 -0300

    Atualizado TBGROUPVEICULOS para ficar igual a classe de dominio

--- LocadoraDeVeiculos/LocadoraDeVeiculos.SqlServer/dbo/Tables/TBGRUPOVEICULO.sql
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.SqlServer/dbo/Tables/TBGRUPOVEICULO.sql
@@ -1,10 +1,14 @@
 ﻿CREATE TABLE [dbo].[TBGRUPOVEICULO] (
-    [Id]                                INT          IDENTITY (1, 1) NOT NULL,
-    [Nome]                              VARCHAR (50) NOT NULL,
-    [TaxaPlanoDiario]                   FLOAT (53)   NOT NULL,
-    [TaxaKmControlado]                  FLOAT (53)   NOT NULL,
-    [TaxaKmLivre]                       FLOAT (53)   NOT NULL,
-    [QuantidadeQuilometrosKmControlado] INT          NOT NULL,
+    [Id]                       INT          IDENTITY (1, 1) NOT NULL,
+    [Nome]                     VARCHAR (50) NOT NULL,
+    [TaxaPlanoDiario]          FLOAT (53)   NOT NULL,
+    [TaxaPorKmDiario]          FLOAT (53)   NOT NULL,
+    [TaxaPlanoControlado]      FLOAT (53)   NOT NULL,
+    [LimiteKmControlado]       INT          NOT NULL,
+    [TaxaKmExcedidoControlado] FLOAT (53)   NOT NULL,
+    [TaxaPlanoLivre]           FLOAT (53)   NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
