dae19e4d55f450b6d72e405c7f2a7bbe937e59a4
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-08-24 15:11:37 -0300

    Classe base ControladorLocacao

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Controladores/LocacaoModule/ControladorLocacao.cs
@@ -0,0 +1,43 @@
+﻿using LocadoraDeVeiculos.Controladores.Shared;
+using LocadoraDeVeiculos.Dominio.LocacaoModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Controladores.LocacaoModule
+{
+    class ControladorLocacao : Controlador<Locacao>
+    {
+        public override string Editar(int id, Locacao registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Existe(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string InserirNovo(Locacao registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Locacao SelecionarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Locacao> SelecionarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
