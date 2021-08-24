3d87f9c382976bb35262cb53e5d8a4533f5131b2
iohan <iohaneduardo1@gmail.com>
2021-08-24 16:45:16 -0300

    estrutura do controlador e dominio feita

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Controladores/ImagemVeiculoModule/ControladorImagemVeiculo.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Controladores/ImagemVeiculoModule/ControladorImagemVeiculo.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LocadoraDeVeiculos.Controladores.Shared;
+using LocadoraDeVeiculos.Controladores.VeiculoModule;
+using LocadoraDeVeiculos.Dominio.ImagemVeiculoModule;
+using LocadoraDeVeiculos.Dominio.VeiculoModule;
 
 namespace LocadoraDeVeiculos.Controladores.ImagemVeiculoModule
 {
-    class ControladorImagemVeiculo
+    public class ControladorImagemVeiculo : Controlador<ImagemVeiculo>
     {
+        #region Queries
+
+        #endregion
+        public override string Editar(int id, ImagemVeiculo registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Existe(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string InserirNovo(ImagemVeiculo registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ImagemVeiculo SelecionarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<ImagemVeiculo> SelecionarTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/ImagemVeiculoModule/ImagemVeiculo.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/ImagemVeiculoModule/ImagemVeiculo.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LocadoraDeVeiculos.Dominio.Shared;
+using LocadoraDeVeiculos.Dominio.VeiculoModule;
 
 namespace LocadoraDeVeiculos.Dominio.ImagemVeiculoModule
 {
-    class ImagemVeiculo
+    public class ImagemVeiculo : EntidadeBase
     {
+        public Veiculo veiculo { get; set; }
+        public Bitmap imagem { get; set; }
+
+        public ImagemVeiculo(int id, Veiculo veiculo, Bitmap imagem)
+        {
+            this.id = id;
+            this.veiculo = veiculo;
+            this.imagem = imagem;
+        }
+
+        public override bool Equals(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1584103619;
+            hashCode = hashCode * -1521134295 + id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<Veiculo>.Default.GetHashCode(veiculo);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Bitmap>.Default.GetHashCode(imagem);
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Validar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
