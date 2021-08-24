a1602dd64e5593fbfb9b3cbe0040cbc4fbbc7940
iohan <iohaneduardo1@gmail.com>
2021-08-24 15:31:36 -0300

    crrigido o banco

--- LocadoraDeVeiculos/LocadoraDeVeiculos.SqlServer/dbo/Tables/TBGRUPOVEICULO.sql
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.SqlServer/dbo/Tables/TBGRUPOVEICULO.sql
@@ -2,13 +2,15 @@
     [Id]                       INT          IDENTITY (1, 1) NOT NULL,
     [Nome]                     VARCHAR (50) NOT NULL,
     [TaxaPlanoDiario]          FLOAT (53)   NOT NULL,
-    [TaxaPorKmDiario]          FLOAT (53)   NOT NULL,
+    [TaxaPorKmDiario]          FLOAT (53)   NULL,
     [TaxaPlanoControlado]      FLOAT (53)   NOT NULL,
-    [LimiteKmControlado]       INT          NOT NULL,
-    [TaxaKmExcedidoControlado] FLOAT (53)   NOT NULL,
+    [LimiteKmControlado]       INT          NULL,
+    [TaxaKmExcedidoControlado] FLOAT (53)   NULL,
     [TaxaPlanoLivre]           FLOAT (53)   NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
+    CONSTRAINT [PK__TBGRUPOV__3214EC07DABBBF2B] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
