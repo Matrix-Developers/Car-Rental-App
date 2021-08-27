c15e48307b34786464687d5b82f2d36df448e53e
iohan <iohaneduardo1@gmail.com>
2021-08-27 14:37:52 -0300

    upado

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.SqlServer/dbo/Tables/TBSERVICO_LOCACAO.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TBSERVICO_LOCACAO] (
+    [id]         INT NOT NULL,
+    [id_locacao] INT NOT NULL,
+    [id_sevico]  INT NOT NULL,
+    CONSTRAINT [id_locacao] FOREIGN KEY ([id_locacao]) REFERENCES [dbo].[TBLOCACAO] ([Id]),
+    CONSTRAINT [id_servico] FOREIGN KEY ([id_sevico]) REFERENCES [dbo].[TBSERVICO] ([Id])
+);
+
