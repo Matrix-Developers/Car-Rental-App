4816a8975e2a9bd51754db3a175010b217f5c49a
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2021-09-03 23:10:22 -0300

    Arquivos iniciais para o módulo "Parceiro" criados.

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.WindowsApp/Features/Parceiros/TabelaParceiroControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraDeVeiculos.WindowsApp.Features.Parceiros
+{
+    public partial class TabelaParceiroControl : UserControl
+    {
+        public TabelaParceiroControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
