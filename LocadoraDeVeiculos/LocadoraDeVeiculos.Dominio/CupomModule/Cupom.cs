e256f0a2e056ee286e41ebd8a36f941c48d5e1a6
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2021-09-04 01:43:40 -0300

    Alterado nome do arquivo do banco; Criado o dominio do Cupom; Testes de dominio realizados;

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/CupomModule/Cupom.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/CupomModule/Cupom.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.ParceiroModule;
+using LocadoraDeVeiculos.Dominio.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,75 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Dominio.CupomModule
 {
-    class Cupom
+    public class Cupom : EntidadeBase
     {
+        public string Nome { get; }
+        public string Codigo { get; }
+        public double Valor { get; }
+        public double ValorMinimo { get; }
+        public bool EhDescontoFixo { get; }
+        public DateTime Validade { get; }
+        public Parceiro Parceiro { get; }
+
+        public Cupom(int id, string nome, string codigo, double valor, double valorMinimo, bool ehDescontoFixo, DateTime validade, Parceiro parceiro)
+        {
+            Id = id;
+            Nome = nome;
+            Codigo = codigo;
+            Valor = valor;
+            ValorMinimo = valorMinimo;
+            EhDescontoFixo = ehDescontoFixo;
+            Validade = validade;
+            Parceiro = parceiro;
+        }
+        public override string Validar()
+        {
+            string resultadoValicadacao = "";
+
+            if (Nome.Length == 0)
+                resultadoValicadacao += "O campo nome é obrigatório\n";
+            if (Codigo.Length == 0)
+                resultadoValicadacao += "O campo código é obrigatório\n";
+            if (Valor <= 0)
+                resultadoValicadacao += "O valor não pode ser negativo ou 0(zero)\n";
+            if(!EhDescontoFixo && Valor > 100)
+                resultadoValicadacao += "A porcentagem de desconto não pode ser maior que 100%\n";
+            if (Parceiro == null)
+                resultadoValicadacao += "É obrigatório possuir um parceiro vinculado\n";
+            if (resultadoValicadacao == "")
+                resultadoValicadacao = "VALIDO";
+            return resultadoValicadacao;
+        }
+        public override string ToString()
+        {
+            return $"[{id}, {Nome}, {Codigo}, {EhDescontoFixo}, {Valor}]";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Cupom cupom &&
+                   Id == cupom.Id &&
+                   Nome == cupom.Nome &&
+                   Codigo == cupom.Codigo &&
+                   Valor == cupom.Valor &&
+                   ValorMinimo == cupom.ValorMinimo &&
+                   EhDescontoFixo == cupom.EhDescontoFixo &&
+                   Validade == cupom.Validade &&
+                   EqualityComparer<Parceiro>.Default.Equals(Parceiro, cupom.Parceiro);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1097376669;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nome);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Codigo);
+            hashCode = hashCode * -1521134295 + Valor.GetHashCode();
+            hashCode = hashCode * -1521134295 + ValorMinimo.GetHashCode();
+            hashCode = hashCode * -1521134295 + EhDescontoFixo.GetHashCode();
+            hashCode = hashCode * -1521134295 + Validade.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<Parceiro>.Default.GetHashCode(Parceiro);
+            return hashCode;
+        }
     }
 }
--- LocadoraDeVeiculos/LocadoraDeVeiculos.Tests/CupomModule/CupomDominioTest.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Tests/CupomModule/CupomDominioTest.cs
@@ -1,12 +1,118 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using LocadoraDeVeiculos.Dominio.CupomModule;
+using LocadoraDeVeiculos.Dominio.ParceiroModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace LocadoraDeVeiculos.Tests.CupomModule
 {
-    class CupomDominioTest
+    [TestClass]
+    [TestCategory("Domínio")]
+    public class CupomDominioTest
     {
+        Parceiro parceiro = new Parceiro(1, "Parceiro teste");
+
+        [TestMethod]
+        public void DeveCriarCupom_Completo_Porcentagem()
+        {
+            //arrange
+            Cupom cupom = new Cupom(0, "Nome Cupom", "NDD10TECH", 10, 2000, false, DateTime.Now.AddDays(10), parceiro);
+
+            //action
+            string resultadoValidacao = cupom.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("VALIDO");
+        }
+
+        [TestMethod]
+        public void DeveCriarCupom_Completo_Fixo()
+        {
+            //arrange
+            Cupom cupom = new Cupom(0, "Nome Cupom", "NDD10TECH", 10, 2000, true, DateTime.Now.AddDays(10), parceiro);
+
+            //action
+            string resultadoValidacao = cupom.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("VALIDO");
+        }
+
+        [TestMethod]
+        public void DeveCriarCupom_TotalmenteIncorreto_Porcentagem()
+        {
+            //arrange
+            Cupom cupom = new Cupom(0, "", "", 101, 0, false, DateTime.Now.AddDays(10), null);
+
+            //action
+            string resultadoValidacao = cupom.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("O campo nome é obrigatório\nO campo código é obrigatório\nA porcentagem de desconto não pode ser maior que 100%\nÉ obrigatório possuir um parceiro vinculado\n");
+        }
+        [TestMethod]
+        public void DeveCriarCupom_TotalmenteIncorreto_Fixo()
+        {
+            //arrange
+            Cupom cupom = new Cupom(0, "", "", 0, 0, true, DateTime.Now.AddDays(10), null);
+
+            //action
+            string resultadoValidacao = cupom.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("O campo nome é obrigatório\nO campo código é obrigatório\nO valor não pode ser negativo ou 0(zero)\nÉ obrigatório possuir um parceiro vinculado\n");
+        }
+
+        [TestMethod]
+        public void DeveApresentarErro_ValorZero_Fixo()
+        {
+            //arrange
+            Cupom cupom = new Cupom(0, "Nome Cupom", "NDD10TECH", 0, 2000, true, DateTime.Now.AddDays(10), parceiro);
+
+            //action
+            string resultadoValidacao = cupom.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("O valor não pode ser negativo ou 0(zero)\n");
+        }
+
+        [TestMethod]
+        public void DeveApresentarErro_PorcentagemMaiorDeCem_Porcentagem()
+        {
+            //arrange
+            Cupom cupom = new Cupom(0, "Nome Cupom", "NDD10TECH", 101, 2000, false, DateTime.Now.AddDays(10), parceiro);
+
+            //action
+            string resultadoValidacao = cupom.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("A porcentagem de desconto não pode ser maior que 100%\n");
+        }
+
+        [TestMethod]
+        public void DeveApresentarErro_NomeVazio()
+        {
+            //arrange
+            Cupom cupom = new Cupom(0, "", "NDD10TECH", 10, 2000, true, DateTime.Now.AddDays(10), parceiro);
+
+            //action
+            string resultadoValidacao = cupom.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("O campo nome é obrigatório\n");
+        }
+
+        [TestMethod]
+        public void DeveApresentarErro_ParceiroNulo()
+        {
+            //arrange
+            Cupom cupom = new Cupom(0, "Nome Cupom", "NDD10TECH", 10, 2000, true, DateTime.Now.AddDays(10), null);
+
+            //action
+            string resultadoValidacao = cupom.Validar();
+
+            //assert
+            resultadoValidacao.Should().Be("É obrigatório possuir um parceiro vinculado\n");
+        }
     }
 }
