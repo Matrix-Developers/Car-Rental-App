bf20f6cda30516b075d6e43773b0b1b273c279c3
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2021-09-04 01:06:15 -0300

    Tabela TBCUPOM_DESCONTO criada.

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.SqlServer/dbo/Tables/TBCUPOMDESCONTO.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[TBCUPOM_DESCONTO]
+(
+	[Id]          INT          IDENTITY (1, 1) NOT NULL,
+    [NomeCupom]   VARCHAR (50) NOT NULL,
+    [Codigo]      VARCHAR (50) NOT NULL,
+    [ValorMinimo] FLOAT (53)   NOT NULL,
+    [Valor]       FLOAT        NOT NULL,    
+    [EhDescontoFixo]        BIT          NOT NULL,
+    [Validade]    DATE         NOT NULL,
+    [Id_Parceiro] INT          NOT NULL,        
+    CONSTRAINT [PK_TBCUPOM_DESCONTO] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_TBDesconto_TBParceiros] FOREIGN KEY ([Id_Parceiro]) REFERENCES [dbo].[TBPARCEIRO] ([Id])
+)
