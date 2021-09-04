63cbcd58de98d2bc283591cf8e366e3186aab9dd
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2021-09-03 23:18:18 -0300

    Dominio Parceiro finalizado.

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/ParceiroModule/Parceiro.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/ParceiroModule/Parceiro.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,46 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Dominio.ParceiroModule
 {
-    class Parceiro
+    public class Parceiro : EntidadeBase
     {
+        public string Nome { get; }
+
+        public Parceiro(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public override string Validar()
+        {
+            string resultadoValidacao = "";
+
+            if (string.IsNullOrEmpty(Nome))
+                resultadoValidacao += "O campo Nome está inválido";
+            if (resultadoValidacao == "")
+                resultadoValidacao = "VALIDO";
+
+            return resultadoValidacao;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Parceiro parceiro &&
+                   Id == parceiro.Id &&
+                   Nome == parceiro.Nome;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1643562096;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nome);
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return $" {id}, {Nome}";
+        }
     }
 }
