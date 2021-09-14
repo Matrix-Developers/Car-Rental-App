97fa5696d03f6d6df4abe4b567dfe58f51844926
Pedroca <pedro.henric.rosario@gmail.com>
2021-09-14 17:31:11 -0300

    Adicionando o ICopumRepository para o dominio

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/CupomModule/ICupomRepository.cs
@@ -0,0 +1,16 @@
+﻿using LocadoraDeVeiculos.Dominio.CupomModule;
+using LocadoraDeVeiculos.Dominio.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.CupomModule
+{
+    public interface ICupomRepository : IRepository<Cupom>
+    {
+        public Cupom SelecionarPorCodigo(string codigo);
+        public void AtualizarQtdUtilizada(int id, int qtdUtilizada);
+    }
+}
