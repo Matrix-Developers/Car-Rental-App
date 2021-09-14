25d9e52884de73353be6a588ae88d0ecdf30b86a
Pedroca <pedro.henric.rosario@gmail.com>
2021-09-14 17:07:12 -0300

    Adicionando IRepository para o domínio

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Dominio/Shared/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.Shared
+{
+    public interface IRepository<T> where T : EntidadeBase
+    {
+        string InserirNovo(T registro);
+        string Editar(int id, T registro);
+        bool Existe(int id);
+        bool Excluir(int id);
+        List<T> SelecionarTodos();
+        T SelecionarPorId(int id);
+    }
+}
