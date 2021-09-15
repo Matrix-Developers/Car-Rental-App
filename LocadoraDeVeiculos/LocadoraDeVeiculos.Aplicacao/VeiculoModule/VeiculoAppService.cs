ffb81064ab574763aa37ac4819c9ed38b30311c1
iohan <iohaneduardo1@gmail.com>
2021-09-15 16:25:55 -0300

    Desenvolvida a classe de VeiculoAppService

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/VeiculoModule/VeiculoAppService.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/VeiculoModule/VeiculoAppService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.Shared;
+using LocadoraDeVeiculos.Dominio.VeiculoModule;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,30 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Aplicacao.VeiculoModule
 {
-    class VeiculoAppService
+    public class VeiculoAppService
     {
+        private readonly IRepository<Veiculo> veiculoRepository;
+
+        public string InserirNovoVeiculo(Veiculo veiculo){
+            string resultadoValidacao = veiculo.Validar();
+
+            if (resultadoValidacao == "VALIDO")
+                veiculoRepository.InserirNovo(veiculo);
+
+            return resultadoValidacao;
+        }
+        public string EditarVeiculo(int id, Veiculo veiculo)
+        {
+            string resultadoValidacao = veiculo.Validar();
+
+            if (resultadoValidacao == "VALIDO")
+                veiculoRepository.Editar(id ,veiculo);
+
+            return resultadoValidacao;
+        }
+        public bool ExcluirVeiculo(int id) { return veiculoRepository.Excluir(id);}
+        public bool ExisteVeiculo(int id) { return veiculoRepository.Existe(id);}
+        public Veiculo SelecionarVeiculoPorId(int id) { return veiculoRepository.SelecionarPorId(id);}
+        public List<Veiculo> SelecionarTodosVeiculo() { return veiculoRepository.SelecionarTodos();}
     }
 }
