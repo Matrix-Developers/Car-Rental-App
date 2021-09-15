0bb33f05f6938c990b9735742d670ec773f9afa4
iohan <iohaneduardo1@gmail.com>
2021-09-15 15:54:18 -0300

    Criada a classe LocacaoAppService

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/LocacaoModule/LocacaoAppService.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/LocacaoModule/LocacaoAppService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.LocacaoModule;
+using LocadoraDeVeiculos.Dominio.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Aplicacao.LocacaoModule
 {
-    class LocacaoAppService
+    public class LocacaoAppService
     {
+        private readonly IRepository<Locacao> locacaoRepository;
+
+        public string InserirNovaLocacao(Locacao locacao){
+            string resultadoValidacao = locacao.Validar();
+
+            if (resultadoValidacao == "VALIDO")
+                locacaoRepository.InserirNovo(locacao);
+
+            return resultadoValidacao;
+        }
+        public string EditarLocacao(int id,Locacao locacao)
+        {
+            string resultadoValidacao = locacao.Validar();
+
+            if (resultadoValidacao == "VALIDO")
+                locacaoRepository.Editar(id, locacao);
+
+            return resultadoValidacao;
+        }
+        public bool ExcluirLocacao(int id) { return locacaoRepository.Excluir(id);}
+        public Locacao SelecionarLocacaoPorId(int id) { return locacaoRepository.SelecionarPorId(id);}
+        public List<Locacao> SelecionarTodosLocacao() { return locacaoRepository.SelecionarTodos();}
+        public bool ExisteLocacao(int id) { return locacaoRepository.Existe(id);}
+
     }
 }
