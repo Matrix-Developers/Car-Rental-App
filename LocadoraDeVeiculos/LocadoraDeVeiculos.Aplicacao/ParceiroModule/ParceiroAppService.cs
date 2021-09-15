c65ee8fbcfb00686cfa484269bfa7505b4fc9b4f
iohan <iohaneduardo1@gmail.com>
2021-09-15 16:05:35 -0300

    Desenvolvida a classe de ParceiroAppService

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/ParceiroModule/ParceiroAppService.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/ParceiroModule/ParceiroAppService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.ParceiroModule;
+using LocadoraDeVeiculos.Dominio.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,30 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Aplicacao.ParceiroModule
 {
-    class ParceiroAppService
+    public class ParceiroAppService
     {
+        private readonly IRepository<Parceiro> parceiroRepository;
+
+        public string InserirNovoParceiro(Parceiro parceiro){
+            string ResultadoValidacao = parceiro.Validar();
+
+            if (ResultadoValidacao == "VALIDO")
+                parceiroRepository.InserirNovo(parceiro);
+
+            return ResultadoValidacao;
+        }
+        public string EditarParceiro(int id, Parceiro parceiro)
+        {
+            string ResultadoValidacao = parceiro.Validar();
+
+            if (ResultadoValidacao == "VALIDO")
+                parceiroRepository.Editar(id,parceiro);
+
+            return ResultadoValidacao;
+        }
+        public void ExcluirParceiro(int id) { parceiroRepository.Excluir(id); }
+        public bool ExisteParceiro(int id) { return parceiroRepository.Existe(id);}
+        public Parceiro SelecionarParceiroPorId(int id) { return parceiroRepository.SelecionarPorId(id);}
+        public List<Parceiro> SelecionarTodosParceiro() { return parceiroRepository.SelecionarTodos();}
     }
 }
