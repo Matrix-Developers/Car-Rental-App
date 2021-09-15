a6a053c9ceef8198703e46a6c5e7f7e4a603ee6e
iohan <iohaneduardo1@gmail.com>
2021-09-15 15:38:59 -0300

    Criada a classe de ImagemVeiculoAppService

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/ImagemVeiculoModule/ImagemVeiculoAppService.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/ImagemVeiculoModule/ImagemVeiculoAppService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.ImagemVeiculoModule;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,34 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Aplicacao.ImagemVeiculoModule
 {
-    class ImagemVeiculoAppService
+    public class ImagemVeiculoAppService
     {
+        private readonly IImagemVeiculoRepository imagemVeiculoRepository;
+
+        public string InserirNovaImagemVeiculo(ImagemVeiculo imagemVeiculo) {
+            string resultadoValidacao = imagemVeiculo.Validar();
+
+            if (resultadoValidacao == "VALIDO")
+                imagemVeiculoRepository.InserirNovo(imagemVeiculo);
+
+            return resultadoValidacao;
+        }
+        public string EditarImagemVeiculo(ImagemVeiculo imagemVeiculo)
+        {
+            string resultadoValidacao = imagemVeiculo.Validar();
+
+            if (resultadoValidacao == "VALIDO")
+                imagemVeiculoRepository.InserirNovo(imagemVeiculo);
+
+            return resultadoValidacao;
+        }
+        public void ExcluirImagemVeiculo(int id) { imagemVeiculoRepository.Excluir(id);}
+        public ImagemVeiculo SelecionarImagemVeiculoPorId(int id) { return imagemVeiculoRepository.SelecionarPorId(id);}
+        public List<ImagemVeiculo> SelecionarTodosImagemVeiculo() { return imagemVeiculoRepository.SelecionarTodos();}
+        public List<ImagemVeiculo> SelecionarTodosImagemVeiculoDeUmVeiculo(int id) { return imagemVeiculoRepository.SelecionarPorIdDoVeiculo(id);}
+        public void EditarListaImagemVeiculo(List<ImagemVeiculo> imagensVeiculo) { imagemVeiculoRepository.EditarLista(imagensVeiculo);}
+        public void ExcluirImagemVeiculoPorIdDoVeiculo(int id) { imagemVeiculoRepository.ExcluirPorIdDoVeiculo(id);}
+
+
     }
 }
