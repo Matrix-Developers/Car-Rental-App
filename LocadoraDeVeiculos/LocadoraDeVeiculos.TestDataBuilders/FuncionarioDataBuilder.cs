15200c10c904f1a8cc8c1b02e0cd86e478787a69
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2021-09-15 15:41:37 -0300

    Criado os seguintes Builders: Cliente; Funcionario; Locacao; Veiculo;
    Foi também criado um construtor vazio nas seguintes classes:
    Cliente;
    Funcionario;
    Locacao;
    Veiculo;

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.TestDataBuilders/FuncionarioDataBuilder.cs
@@ -0,0 +1,80 @@
+﻿using LocadoraDeVeiculos.Dominio.FuncionarioModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.TestDataBuilders
+{
+    public class FuncionarioDataBuilder
+    {
+        private Funcionario funcionario;
+
+        public FuncionarioDataBuilder()
+        {
+            funcionario = new Funcionario();
+        }
+
+        public FuncionarioDataBuilder ComNome(string nome)
+        {
+            funcionario.Nome = nome;
+            return this;
+        }
+        public FuncionarioDataBuilder ComRegistroUnico(string registroUnico)
+        {
+            funcionario.RegistroUnico = registroUnico;
+            return this;
+        }
+        public FuncionarioDataBuilder ComEndereco(string endereco)
+        {
+            funcionario.Endereco = endereco;
+            return this;
+        }
+        public FuncionarioDataBuilder ComTelefone(string telefone)
+        {
+            funcionario.Telefone = telefone;
+            return this;
+        }
+        public FuncionarioDataBuilder ComEmail(string email)
+        {
+            funcionario.Email = email;
+            return this;
+        }
+        public FuncionarioDataBuilder ComMatriculaInterna(int matriculaInterna)
+        {
+            funcionario.MatriculaInterna = matriculaInterna;
+            return this;
+        }
+        public FuncionarioDataBuilder ComUsuarioAcesso(string usuarioAcesso)
+        {
+            funcionario.UsuarioAcesso= usuarioAcesso;
+            return this;
+        }
+        public FuncionarioDataBuilder ComSenha(string senha)
+        {
+            funcionario.Senha = senha;
+            return this;
+        }
+        public FuncionarioDataBuilder ComDataAdmissao(DateTime dataAdmissao)
+        {
+            funcionario.DataAdmissao = dataAdmissao;
+            return this;
+        }
+        public FuncionarioDataBuilder ComCargo(string cargo)
+        {
+            funcionario.Cargo = cargo;
+            return this;
+        }
+        public FuncionarioDataBuilder ComSalario(double salario)
+        {
+            funcionario.Salario = salario;
+            return this;
+        }
+        public FuncionarioDataBuilder ComEhPessoaFisica(bool ehPessoaFisica)
+        {
+            funcionario.EhPessoaFisica = ehPessoaFisica;
+            return this;
+        }
+    }
+}
