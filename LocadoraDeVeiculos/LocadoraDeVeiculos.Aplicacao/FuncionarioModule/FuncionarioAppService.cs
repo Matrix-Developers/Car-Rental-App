02a6b4c279235bf80ad81dfbd664fc956264daca
iohan <iohaneduardo1@gmail.com>
2021-09-15 14:32:40 -0300

    Desenvolvida a classe de FuncionarioAppService

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/FuncionarioModule/FuncionarioAppService.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/FuncionarioModule/FuncionarioAppService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.FuncionarioModule;
+using LocadoraDeVeiculos.Dominio.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace LocadoraDeVeiculos.Aplicacao.FuncionarioModule
 {
     public class FuncionarioAppService
     {
+        private readonly IRepository<Funcionario> funcionarioRepository;
+
+        public string InserirNovoFuncionario(Funcionario funcionario){
+            string resultadoValidacao = funcionario.Validar();
+            if (resultadoValidacao == "VALIDO")
+                funcionarioRepository.InserirNovo(funcionario);
+            return resultadoValidacao;
+        }
+        public string EditarFuncionario(int id, Funcionario funcionario){
+            string resultadoValidacao = funcionario.Validar();
+            if (resultadoValidacao == "VALIDO")
+                funcionarioRepository.Editar(id,funcionario);
+            return resultadoValidacao;
+        }
+        public void ExcluirFuncionario(int id) { funcionarioRepository.Excluir(id);}
+        public void ExisteFuncionario(int id) { funcionarioRepository.Existe(id);}
+        public Funcionario SelecionarFuncionarioPorId(int id) { return funcionarioRepository.SelecionarPorId(id);}
+        public List<Funcionario> SelecionarTodos() { return funcionarioRepository.SelecionarTodos();}
     }
 }
