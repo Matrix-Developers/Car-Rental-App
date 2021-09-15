87c965421e23a49ed74bdfb332d9cf01714c4774
iohan <iohaneduardo1@gmail.com>
2021-09-15 16:14:31 -0300

    Desenvolvida a classe RelacionamentoLocServ.App.Service

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/RelacionamentoLocServModule/RelacionamentoLocServAppService.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/RelacionamentoLocServModule/RelacionamentoLocServAppService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.RelacionamentoLocServModule;
+using LocadoraDeVeiculos.Dominio.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Aplicacao.RelacionamentoLocServModule
 {
-    class RelacionamentoLocServAppService
+    public class RelacionamentoLocServAppService
     {
+        private readonly IRepository<RelacionamentoLocServ> relacionamentoLocServRepository;
+
+        public string InserirNovoRelacionamentoLocServ(RelacionamentoLocServ relacionamentoLocServ){
+            string resultadoValidacao = relacionamentoLocServ.Validar();
+
+            if (resultadoValidacao == "VALIDO")
+                relacionamentoLocServRepository.InserirNovo(relacionamentoLocServ);
+
+            return resultadoValidacao;
+        }
+        public string EditarRelacionamentoLocServ(int id, RelacionamentoLocServ relacionamentoLocServ)
+        {
+            throw new NotImplementedException();
+        }
+        public bool ExcluirRelacionamentoLocServ(int id) { return relacionamentoLocServRepository.Excluir(id);}
+        public bool ExisteRelacionamentoLocServ(int id) { return relacionamentoLocServRepository.Existe(id);}
+        public RelacionamentoLocServ SelecionarRelacionamentoLocServPorId(int id) { return relacionamentoLocServRepository.SelecionarPorId(id);}
+        public List<RelacionamentoLocServ> SelecionarTodosRelacionamentoLocServ() { return relacionamentoLocServRepository.SelecionarTodos();}
+
     }
 }
