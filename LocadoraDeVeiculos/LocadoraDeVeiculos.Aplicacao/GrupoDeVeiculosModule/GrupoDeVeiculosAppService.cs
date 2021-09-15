4fe0878e86160d80bcd0130defea0598dc00c314
iohan <iohaneduardo1@gmail.com>
2021-09-15 14:42:09 -0300

    Desenvolvida a classe de GrupoDeVeiculosAppService

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/GrupoDeVeiculosModule/GrupoDeVeiculosAppService.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/GrupoDeVeiculosModule/GrupoDeVeiculosAppService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.Shared;
+using LocadoraDeVeiculos.Dominio.GrupoDeVeiculosModule;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Aplicacao.GrupoDeVeiculosModule
 {
-    class GrupoDeVeiculosAppService
+    public class GrupoDeVeiculosAppService
     {
+        private readonly IRepository<GrupoDeVeiculo> grupoDeVeiculoRepository;
+
+        public string InserirNovoGrupoDeVeiculo(GrupoDeVeiculo grupoDeVeiculos){
+            string resultadoValidacao = grupoDeVeiculos.Validar();
+
+            if (resultadoValidacao == "VALIDO")
+                grupoDeVeiculoRepository.InserirNovo(grupoDeVeiculos);
+
+            return resultadoValidacao;
+        }
+        public string EditarGrupoDeVeiculo(int id,GrupoDeVeiculo grupoDeVeiculos)
+        {
+            string resultadoValidacao = grupoDeVeiculos.Validar();
+
+            if (resultadoValidacao == "VALIDO")
+                grupoDeVeiculoRepository.Editar(id,grupoDeVeiculos);
+
+            return resultadoValidacao;
+        }
+        public void ExcluirGrupoDeVeiculo(int id) { grupoDeVeiculoRepository.Excluir(id);}
+        public bool ExisteGrupoDeVeiculo(int id) { return grupoDeVeiculoRepository.Existe(id);}
+        public GrupoDeVeiculo SelecionarGrupoDeVeiculoPorId(int id) { return grupoDeVeiculoRepository.SelecionarPorId(id);}
+        public List<GrupoDeVeiculo> SelecionarTodosGrupoDeVeiculo() { return grupoDeVeiculoRepository.SelecionarTodos();}
+        
     }
 }
