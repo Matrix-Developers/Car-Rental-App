e420e32da2e548d4765550e6f51ac5a269d8dd29
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2021-09-24 01:48:02 -0300

    Criado os arquivos.

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Infra.Logs/GeradorLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LocadoraDeVeiculos.Infra.Logs
+{
+    public abstract class GeradorLog
+    {
+        static void GerarLog(string mensagem, string nivelLog)
+        {
+            using (var logger = EnviarLog())
+                logger.Write(LogEventLevel.Error, mensagem);
+        }
+
+        private static Logger EnviarLog()
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.Seq("http://localhost:5341")
+                .CreateLogger();
+            Log.Logger = logger;
+            return logger;
+        }
+    }
+}
