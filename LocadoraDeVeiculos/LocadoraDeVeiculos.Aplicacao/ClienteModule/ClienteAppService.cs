c5c9af221cf6ee9e85c71bc04807b753f756d2c2
iohan <iohaneduardo1@gmail.com>
2021-09-27 01:53:12 -0300

    clientes ok

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/ClienteModule/ClienteAppService.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/ClienteModule/ClienteAppService.cs
@@ -1,6 +1,8 @@
 ﻿using LocadoraDeVeiculos.Aplicacao.Shared;
 using LocadoraDeVeiculos.Dominio.ClienteModule;
 using LocadoraDeVeiculos.Dominio.Shared;
+using Serilog;
+using System;
 using System.Collections.Generic;
 
 namespace LocadoraDeVeiculos.Aplicacao.ClienteModule
@@ -16,19 +18,35 @@ namespace LocadoraDeVeiculos.Aplicacao.ClienteModule
 
         public override bool InserirEntidade(Cliente cliente)
         {
-           bool resultado = clienteRepository.InserirNovo(cliente);
+            bool resultado = clienteRepository.InserirNovo(cliente);
+
+            if (resultado)
+                Log.Information("{DataEHora} / Cliente {cliente} adicionado com sucesso", DateTime.Now, cliente);
+            else
+                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: ?????", DateTime.Now, this.ToString(), "AppService", "Inserir", cliente);
 
             return resultado;
         }
         public override bool EditarEntidade(int id, Cliente cliente)
         {
             bool resultado = clienteRepository.Editar(id, cliente);
+            if (resultado)
+                Log.Information("{DataEHora} / Cliente {cliente} editado com sucesso", DateTime.Now, cliente);
+            else
+                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: ?????", DateTime.Now, this.ToString(), "AppService", "Inserir", cliente);
 
             return resultado;
         }
         public override bool ExcluirEntidade(int id)
         {
-            return clienteRepository.Excluir(id);
+            bool resultado = clienteRepository.Excluir(id);
+
+            if (resultado)
+                Log.Information("{DataEHora} / Cliente {id} excluido com sucesso", DateTime.Now, id);
+            else
+                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: ?????", DateTime.Now, this.ToString(), "AppService", "Inserir", id);
+
+            return resultado;
         }
         public override bool ExisteEntidade(int id)
         {
