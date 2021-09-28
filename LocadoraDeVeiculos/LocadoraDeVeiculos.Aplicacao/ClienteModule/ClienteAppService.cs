61064c1900dbd075f9ac89a6c7bf37bac3566eb2
iohan <iohaneduardo1@gmail.com>
2021-09-28 16:32:39 -0300

    cliente ok

--- LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/ClienteModule/ClienteAppService.cs
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Aplicacao/ClienteModule/ClienteAppService.cs
@@ -10,7 +10,7 @@ namespace LocadoraDeVeiculos.Aplicacao.ClienteModule
     public class ClienteAppService : AppServiceBase<Cliente>
     {
         private readonly IRepository<Cliente> clienteRepository;
-
+        long tempo;
         public ClienteAppService(IRepository<Cliente> clienteRepository)
         {
             this.clienteRepository = clienteRepository;
@@ -18,33 +18,40 @@ namespace LocadoraDeVeiculos.Aplicacao.ClienteModule
 
         public override bool InserirEntidade(Cliente cliente)
         {
+            tempo = DateTime.Now.Millisecond;
             bool resultado = clienteRepository.InserirNovo(cliente);
+            tempo = DateTime.Now.Millisecond - tempo;
 
             if (resultado)
                 Log.Information("{DataEHora} / Cliente {cliente} adicionado com sucesso", DateTime.Now, cliente);
             else
-                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: ?????", DateTime.Now, this.ToString(), "AppService", "Inserir", cliente);
+                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: {Tempo}ms", DateTime.Now, this.ToString(), "AppService", "Inserir", cliente, tempo);
 
             return resultado;
         }
         public override bool EditarEntidade(int id, Cliente cliente)
         {
+            tempo = DateTime.Now.Millisecond;
             bool resultado = clienteRepository.Editar(id, cliente);
+            tempo = DateTime.Now.Millisecond - tempo;
+
             if (resultado)
                 Log.Information("{DataEHora} / Cliente {cliente} editado com sucesso", DateTime.Now, cliente);
             else
-                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: ?????", DateTime.Now, this.ToString(), "AppService", "Editar", cliente);
+                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: {Tempo}ms", DateTime.Now, this.ToString(), "AppService", "Editar", cliente, tempo);
 
             return resultado;
         }
         public override bool ExcluirEntidade(int id)
         {
+            tempo = DateTime.Now.Millisecond;
             bool resultado = clienteRepository.Excluir(id);
+            tempo = DateTime.Now.Millisecond - tempo;
 
             if (resultado)
                 Log.Information("{DataEHora} / Cliente {id} excluido com sucesso", DateTime.Now, id);
             else
-                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: ?????", DateTime.Now, this.ToString(), "AppService", "Excluir", id);
+                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: {Tempo}ms", DateTime.Now, this.ToString(), "AppService", "Excluir", id, tempo);
 
             return resultado;
         }
@@ -54,22 +61,26 @@ namespace LocadoraDeVeiculos.Aplicacao.ClienteModule
         }
         public override Cliente SelecionarEntidadePorId(int id)
         {
+            tempo = DateTime.Now.Millisecond;
             Cliente cliente = clienteRepository.SelecionarPorId(id);
+            tempo = DateTime.Now.Millisecond - tempo;
 
             if (cliente != null)
                 Log.Information("{DataEHora} / Cliente {id} Selecionado com sucesso", DateTime.Now, id);
             else
-                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: ?????", DateTime.Now, this.ToString(), "AppService", "Selecionar por id", id);
+                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: {Tempo}ms", DateTime.Now, this.ToString(), "AppService", "Selecionar por id", id, tempo);
 
             return cliente;
         }
         public override List<Cliente> SelecionarTodasEntidade()
         {
+            tempo = DateTime.Now.Millisecond;
             List<Cliente> clientes = clienteRepository.SelecionarTodos();
-            if(clientes != null)
+            tempo = DateTime.Now.Millisecond - tempo;
+            if (clientes != null)
                 Log.Information("{DataEHora} / Cliente {id} Selecionado com sucesso", DateTime.Now, clientes.Count);
             else
-                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: ?????", DateTime.Now, this.ToString(), "AppService", "Inserir", clientes.Count);
+                Log.Error("{DataEHora} / Feature: {Feature} / Camada: {Camada} / Módulo: {Módulo} / Registro: {Id} / Tempo total: {Tempo}ms", DateTime.Now, this.ToString(), "AppService", "Inserir", clientes.Count, tempo);
             return clientes;
         }
     }
