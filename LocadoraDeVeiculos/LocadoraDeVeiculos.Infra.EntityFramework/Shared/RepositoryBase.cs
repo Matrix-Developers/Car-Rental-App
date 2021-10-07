2e270e3f147d8cf5f1b23242f21b9fbb604772f6
iohan <iohaneduardo1@gmail.com>
2021-10-07 17:39:00 -0300

    criado repositorybase da ORM

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Infra.EntityFramework/Shared/RepositoryBase.cs
@@ -0,0 +1,103 @@
+﻿using LocadoraDeVeiculos.Dominio.Shared;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.EntityFramework.Shared
+{
+    public abstract class RepositoryBase<T> where T : EntidadeBase
+    {
+        protected readonly LocadoraDeVeiculosDBContext db;
+        protected readonly DbSet<T> dbSet;
+
+        public bool InserirNovo(T registro)
+        {
+            try
+            {
+                dbSet.Add(registro);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("{DataEHora} / Ocorreu um erro ao tentar Inserir um(a) novo(a) {Feature} / Camada: Repository / Usuário: IdUsuario / Sql: query / {StackTrace}", DateTime.Now, this.ToString(), ex);
+                return false;
+            }
+
+            return true;
+        }
+        public bool Editar(int id, T registro)
+        {
+            try
+            {
+                registro.Id = id;
+                dbSet.Update(registro);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("{DataEHora} / Ocorreu um erro ao tentar Editar  um(a) {Feature} / Camada: Repository / Id Processo: {IdProcesso} / Usuário: IdUsuario / Sql: query / {StackTrace}", DateTime.Now, this.ToString(), id, ex);
+                return false;
+            }
+            return true;
+        }
+        public bool Excluir(int id)
+        {
+            try
+            {
+                T registro = SelecionarPorId(id);
+                if (registro != null)
+                {
+                    dbSet.Remove(registro);
+                    db.SaveChanges();
+                }
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("{DataEHora} / Ocorreu um erro ao tentar Excluir um(a) {Feature} / Camada: Repository / Id Processo: {IdProcesso} / Usuário: IdUsuario / Sql: query / {StackTrace}", DateTime.Now, this.ToString(), id, ex);
+                return false;
+            }
+            return true;
+        }
+        public T SelecionarPorId(int id)
+        {
+            try
+            {
+                return dbSet.FirstOrDefault(x => x.Id.Equals(id));
+            }
+            catch (Exception ex)
+            {
+                Log.Error("{DataEHora} / Ocorreu um erro ao tentar Selecionar Por Id um(a) {Feature} / Camada: Repository / Id Processo: {IdProcesso} / Usuário: IdUsuario / Sql: query / {StackTrace}", DateTime.Now, this.ToString(), id, ex);
+                return null;
+            }
+        }
+        public List<T> SelecionarTodos()
+        {
+            try
+            {
+                return dbSet.ToList<T>();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("{DataEHora} / Ocorreu um erro ao tentar Selecionar Todos um(a) {Feature} / Camada: Repository / Usuário: IdUsuario / Sql: query / {StackTrace}", DateTime.Now, this.ToString(), ex);
+                return null;
+            }
+        }
+        public bool Existe(int id)
+        {
+            try
+            {
+                return SelecionarPorId(id) != null;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("{DataEHora} / Ocorreu um erro ao tentar Verificar se Existe o(a) {Feature} / Camada: Repository / Usuário: IdUsuario / Sql: query / {StackTrace}", DateTime.Now, this.ToString(), ex);
+                return false;
+            }
+        }
+    }
+}
