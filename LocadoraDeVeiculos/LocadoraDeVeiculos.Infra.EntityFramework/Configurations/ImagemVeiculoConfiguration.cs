4e8abf87477a10f91a1851b974a7f824af89adf2
iohan <iohaneduardo1@gmail.com>
2021-10-08 18:02:57 -0300

    Imagem ok

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Infra.EntityFramework/Configurations/ImagemVeiculoConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using LocadoraDeVeiculos.Dominio.ImagemVeiculoModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeVeiculos.Infra.EntityFramework.Configurations
+{
+    public class ImagemVeiculoConfiguration : IEntityTypeConfiguration<ImagemVeiculo>
+    {
+        public void Configure(EntityTypeBuilder<ImagemVeiculo> builder)
+        {
+            builder.ToTable("TBIMAGEMVEICULO");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property("ID_VEICULO").HasColumnType("INT");
+            builder.Property("imagem").HasColumnType("VARBINARY(MAX)");
+
+            builder.HasOne(p => p.veiculo).WithMany(p => p.imagens).HasForeignKey(p => p.IdVeiculo);
+        }
+    }
+}
