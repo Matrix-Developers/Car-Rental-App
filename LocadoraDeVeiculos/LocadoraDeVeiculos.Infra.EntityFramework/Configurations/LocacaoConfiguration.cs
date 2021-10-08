a2bbe5335075bed6736fa062316f29428ebe5a5c
Pedroca <pedro.henric.rosario@gmail.com>
2021-10-08 17:57:27 -0300

    Classes Configuration do EF (WIP)

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Infra.EntityFramework/Configurations/LocacaoConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using LocadoraDeVeiculos.Dominio.LocacaoModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeVeiculos.Infra.EntityFramework.Configurations
+{
+    class LocacaoConfiguration : IEntityTypeConfiguration<Locacao>
+    {
+        public void Configure(EntityTypeBuilder<Locacao> builder)
+        {
+            builder.ToTable("TBLOCACAO");
+            builder.HasKey(p => p.Id);
+
+            //private Veiculo veiculo;
+            //private Funcionario funcionarioLocador;
+            //private Cliente clienteContratante;
+            //private Cliente clienteCondutor;
+            //private Cupom cupom;
+            //private DateTime dataDeSaida;
+            //private DateTime dataPrevistaDeChegada;
+            //private DateTime dataDeChegada;
+            //private string tipoDoPlano;         //PlanoDiario, KmControlado ou KmLivre
+            //private string tipoDeSeguro;    //SeguroCliente, SeguroTerceiro ou Nenhum
+            //private double precoLocacao;
+            //private double precoDevolucao;
+            //private bool estaAberta;
+            //private List<Servico> servicos;
+
+            //builder.Property(p => p.Nome).HasColumnType("VARCHAR(50)").IsRequired();
+            //builder.Property(p => p.Codigo).HasColumnType("VARCHAR(50)").IsRequired();
+            //builder.Property(p => p.Valor).HasColumnType("FLOAT").IsRequired();
+            //builder.Property(p => p.ValorMinimo).HasColumnType("FLOAT").IsRequired();
+            //builder.Property(p => p.EhDescontoFixo).HasColumnType("BIT").IsRequired();
+            //builder.Property(p => p.Validade).HasColumnType("DATE").IsRequired();
+            //builder.Property(p => p.ParceiroId).HasColumnType("INT");
+            //builder.Property(p => p.QtdUtilizada).HasColumnType("INT").IsRequired();
+
+            //builder.HasOne(p => p.Parceiro).WithMany(p => p.Cupons).HasForeignKey(p => p.ParceiroId);
+        }
+    }
+}
---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Infra.EntityFramework/Configurations/RelacionamentoLocServConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using LocadoraDeVeiculos.Dominio.RelacionamentoLocServModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeVeiculos.Infra.EntityFramework.Configurations
+{
+    class RelacionamentoLocServConfiguration : IEntityTypeConfiguration<RelacionamentoLocServ>
+    {
+        public void Configure(EntityTypeBuilder<RelacionamentoLocServ> builder)
+        {
+            builder.ToTable("TBRELACIONAMENTOLOCSERV");
+            builder.HasKey(p => p.Id);
+
+            //public Locacao Locacao { get; }
+            //public List<Servico> Servicos { get; }
+            //public int IdServico { get; }
+
+            //builder.Property(p => p.Nome).HasColumnType("VARCHAR(50)").IsRequired();
+            //builder.Property(p => p.Codigo).HasColumnType("VARCHAR(50)").IsRequired();
+            //builder.Property(p => p.Valor).HasColumnType("FLOAT").IsRequired();
+            //builder.Property(p => p.ValorMinimo).HasColumnType("FLOAT").IsRequired();
+            //builder.Property(p => p.EhDescontoFixo).HasColumnType("BIT").IsRequired();
+            //builder.Property(p => p.Validade).HasColumnType("DATE").IsRequired();
+            //builder.Property(p => p.ParceiroId).HasColumnType("INT");
+            //builder.Property(p => p.QtdUtilizada).HasColumnType("INT").IsRequired();
+
+            //builder.HasOne(p => p.Parceiro).WithMany(p => p.Cupons).HasForeignKey(p => p.ParceiroId);
+        }
+    }
+}
