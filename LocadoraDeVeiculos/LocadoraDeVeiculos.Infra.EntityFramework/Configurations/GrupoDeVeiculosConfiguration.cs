4190c4b32996a7c44ff3913f7838ab734383feff
iohan <iohaneduardo1@gmail.com>
2021-10-08 17:00:56 -0300

    removendo arquivos desnecessários

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos.Infra.EntityFramework/Configurations/GrupoDeVeiculosConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using LocadoraDeVeiculos.Dominio.GrupoDeVeiculosModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeVeiculos.Infra.EntityFramework.Configurations
+{
+    public class GrupoDeVeiculosConfiguration : IEntityTypeConfiguration<GrupoDeVeiculo>
+    {
+        public void Configure(EntityTypeBuilder<GrupoDeVeiculo> builder)
+        {
+            builder.ToTable("");
+        }
+    }
+}
