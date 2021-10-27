23c7a17597c144f9b7e9db4abda1057e2064d4da
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2021-10-27 17:41:58 -0300

    Corrigido erro de duas threads utilizando o mesmo context.

--- LocadoraDeVeiculos/EnvioDeEmailWorker/EncaminhaEmailWorker.cs
+++ LocadoraDeVeiculos/EnvioDeEmailWorker/EncaminhaEmailWorker.cs
@@ -19,7 +19,7 @@ namespace LocadoraDeVeiculos.Infra.ApiServices
     public class EncaminhaEmailWorker : BackgroundService
     {
 
-        LocacaoAppService locacaoAppService = new(new LocacaoORM(new LocadoraDeVeiculosDBContext()));
+        LocacaoAppService locacaoAppService;// = new(new LocacaoORM(new LocadoraDeVeiculosDBContext()));
         public readonly ILogger<EncaminhaEmailWorker> _logger;
         Stopwatch stopwatch = new();
         public static string DiretorioEntrada { get; set; }
@@ -36,7 +36,7 @@ namespace LocadoraDeVeiculos.Infra.ApiServices
                 {
                     stopwatch.Start();
                     Parallel.ForEach(ColetaDiretorio(), (diretorio) => Program.ArquivosPdfEncontrados.Enqueue(diretorio));
-                    Parallel.ForEach(Program.ArquivosPdfEncontrados, (arquivoPdf) => EnviarEmail(arquivoPdf));
+                    Parallel.ForEach(Program.ArquivosPdfEncontrados, (arquivoPdf) => EnviarEmail(arquivoPdf, new LocadoraDeVeiculosDBContext()));
                     //foreach (var item in ColetaDiretorio())
                     //{
                     //    string arquivoPdf = "";
@@ -46,25 +46,44 @@ namespace LocadoraDeVeiculos.Infra.ApiServices
                 }
                 catch (Exception ex)
                 {
-
-                    throw;
+                    _logger.LogError("Erro: {exception} Tempo decorrido: {stopwatch}", ex, stopwatch.ElapsedMilliseconds / 1000.0);
                 }
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(5000, stoppingToken);
             }
         }
 
-        private void EnviarEmail(string arquivoPdf)
+        private void EnviarEmail(string arquivoPdf, LocadoraDeVeiculosDBContext locadoraDeVeiculosDBContext)
         {
-            int idLocacacao = Convert.ToInt32(arquivoPdf.Replace("..\\..\\..\\..\\Recibos\\recibo-", "").Replace(".pdf", ""));
-            Locacao locacao = locacaoAppService.SelecionarEntidadePorId(idLocacacao);
+            try
+            {
+                int idLocacacao = Convert.ToInt32(arquivoPdf.Replace("..\\..\\..\\..\\Recibos\\recibo-", "").Replace(".pdf", ""));
+                locacaoAppService = new LocacaoAppService(new LocacaoORM(locadoraDeVeiculosDBContext));
+                Locacao locacao = locacaoAppService.SelecionarEntidadePorId(idLocacacao);
+                GerenciadorDeEmail.EnviarEmail("matriquisdevelopers@gmail.com", "matrixadm", locacao);
 
-            GerenciadorDeEmail.EnviarEmail("matriquisdevelopers@gmail.com", "matrixadm", locacao);
+                File.Copy(arquivoPdf, arquivoPdf.Replace("\\Recibos\\", "\\Enviados\\"));
+                _logger.LogInformation("Arquivo: {arquivo} movido para a pasta Enviados", arquivoPdf.Replace("..\\..\\..\\..\\Recibos\\", ""), stopwatch.ElapsedMilliseconds / 1000.0);
+                File.Delete(arquivoPdf);
+                _logger.LogInformation("Arquivo: {arquivo} removido da pasta Pdf  com suceso, Tempo decorrido: {stopwatch}", arquivoPdf.Replace("..\\..\\..\\..\\Recibos\\", ""), stopwatch.ElapsedMilliseconds / 1000.0);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Erro: {exception} Tempo decorrido: {stopwatch}", ex, stopwatch.ElapsedMilliseconds / 1000.0);
+                if (File.Exists(arquivoPdf))
+                {
+                    File.Copy(arquivoPdf, arquivoPdf.Replace("\\Recibos\\", "\\Erro\\"));
+                    _logger.LogInformation("Arquivo: {arquivo} movido para a pasta Erro, Tempo decorrido: {stopwatch}", arquivoPdf.Replace("..\\..\\..\\..\\Recibos\\", ""), stopwatch.ElapsedMilliseconds / 1000.0);
+                    File.Delete(arquivoPdf);
+                    _logger.LogInformation("Arquivo: {arquivo} removido da pasta Pdf  com suceso, Tempo decorrido: {stopwatch}", arquivoPdf.Replace("..\\..\\..\\..\\Recibos\\", ""), stopwatch.ElapsedMilliseconds / 1000.0);
+                }
+            }
+            stopwatch.Stop();
         }
 
         public string[] ColetaDiretorio()
         {
-            string[] arquivosEncontrados =  Directory.GetFiles($@"..\..\..\..\Recibos\", "*.pdf");
+            string[] arquivosEncontrados = Directory.GetFiles($@"..\..\..\..\Recibos\", "*.pdf");
             _logger.LogInformation("{quantida} arquivos encontrados, Hora: {time}, Tempo Decorrido: {stopWatch}", arquivosEncontrados.Length, DateTimeOffset.Now, stopwatch.ElapsedMilliseconds / 1000.0);
             return arquivosEncontrados;
         }
